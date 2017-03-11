b68ffb292c4a411695a2aa59d27963599a09d6a0
HenryVegaAyala <hp.vega@hotmail.com>
2017-03-11 16:08:00 -0500

    Alsina

--- config/params.php
+++ config/params.php
@@ -1,5 +1,11 @@
 <?php
 
+/*PRODUCCION*/
+//Yii::setAlias('@groupgygUrlError','http://pasaporte.rusticaclub.com/help/alsina.png');
+
+/*LOCAL*/
+Yii::setAlias('@alsinaLogo','http://localhost/alsina/help/alsina.png');
+
 return [
     'adminEmail' => 'admin@example.com',
 
