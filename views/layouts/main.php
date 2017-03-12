6f2e0c182e1518b2886117f36fa2aefa471a48f3
HenryVegaAyala <hp.vega@hotmail.com>
2017-03-11 20:06:07 -0500

    Alsina

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -32,6 +32,8 @@ if (Yii::$app->controller->action->id === 'login') {
     <head>
         <meta charset="<?= Yii::$app->charset ?>"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="robots" content="noindex, nofollow">
+        <link rel="shortcut icon" href="<?php echo Yii::$app->getHomeUrl(); ?>/favicon.ico" type="image/x-icon" />
         <?= Html::csrfMetaTags() ?>
         <title><?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
