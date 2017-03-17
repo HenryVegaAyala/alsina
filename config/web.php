eaa0126d4d8edcf48dd5b5634dcab9367f17e2e7
Henry <hvega@dtransforma.com>
2017-03-17 17:30:00 -0500

    Alsina

--- config/web.php
+++ config/web.php
@@ -139,9 +139,7 @@ $config = [
 
                 /** Usuario **/
                 ['pattern' => '/actualizar/<id:\d+>', 'route' => '/usuario/update'],
-
-
-
+                
             ],
         ],
 
