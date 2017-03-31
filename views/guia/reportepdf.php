2395b279852ac238596892cd4c0c8a1ff46aa7ff
Henry <hvega@dtransforma.com>
2017-03-31 13:58:37 -0500

    Alsina

--- views/guia/reportepdf.php
+++ views/guia/reportepdf.php
@@ -36,14 +36,14 @@ class PDF extends FPDF
         $this->Cell(0.6, 0.45, utf8_decode("#"), 1, '', 'C');
         $this->Cell(1.5, 0.45, utf8_decode("Código"), 1, '', 'C');
         $this->Cell(6.7, 0.45, utf8_decode("Elementos"), 1, '', 'C');
-        $this->Cell(1.5, 0.45, utf8_decode("P. x dia"), 1, '', 'C');
-        $this->Cell(1.5, 0.45, utf8_decode("P. Real"), 1, '', 'C');
-        $this->Cell(1.5, 0.45, utf8_decode("P. Volum."), 1, '', 'C');
-        $this->Cell(1.5, 0.45, utf8_decode("Ud."), 1, '', 'C');
-        $this->Cell(1.5, 0.45, utf8_decode("P. R. Total"), 1, '', 'C');
-        $this->Cell(1.5, 0.45, utf8_decode("Cant. Dias"), 1, '', 'C');
-        $this->Cell(1.5, 0.45, utf8_decode("Costo total"), 1, '', 'C');
-        $this->Cell(1.5, 0.45, utf8_decode("P. V. Total"), 1, '', 'C');
+        $this->Cell(1.4, 0.45, utf8_decode("P. x dia"), 1, '', 'C');
+        $this->Cell(1.4, 0.45, utf8_decode("P. Real"), 1, '', 'C');
+        $this->Cell(1.4, 0.45, utf8_decode("P. Volum."), 1, '', 'C');
+        $this->Cell(1, 0.45, utf8_decode("Ud."), 1, '', 'C');
+        $this->Cell(1.4, 0.45, utf8_decode("P. R. Total"), 1, '', 'C');
+        $this->Cell(1.4, 0.45, utf8_decode("Cant. Dias"), 1, '', 'C');
+        $this->Cell(1.4, 0.45, utf8_decode("Costo total"), 1, '', 'C');
+        $this->Cell(1.4, 0.45, utf8_decode("P. V. Total"), 1, '', 'C');
         $this->Ln();
 
         $connection = \Yii::$app->db;
