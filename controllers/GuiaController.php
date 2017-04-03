c31d2b176d0e6dbdf3a96783780ca8f9a5a1b6e9
Henry <hvega@dtransforma.com>
2017-04-03 16:30:37 -0500

    Alsina

--- controllers/GuiaController.php
+++ controllers/GuiaController.php
@@ -361,8 +361,8 @@ class GuiaController extends Controller
         } else {
             $codigo = $_POST['numeroobra'];
         }
-
-        $lista = $guia->ListaGuia($codigo);
+//        $lista = $guia->ListaGuia($codigo);
+        $lista = \yii\helpers\ArrayHelper::map(Guia::find()->where("COD_ESTA = 1 and NUM_OBRA = '" . $codigo . "'")->all(), 'NUM_GUIA', 'NUM_GUIA');
         echo "<option value=\"\">Seleccionar una Guía</option>";
         foreach ($lista as $data):
             echo "<option value=\"{$data->NUM_GUIA}\">{$data->NUM_GUIA}</option>";
