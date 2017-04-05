35574c5d93bb71d3031280b47c8beac8bb26346f
Henry <hvega@dtransforma.com>
2017-04-05 17:02:53 -0500

    Alsina

--- models/Guia.php
+++ models/Guia.php
@@ -41,8 +41,8 @@ class Guia extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['FECH_LLEGA', 'FECH_CORTE','NUMERO_GUIA'], 'required'],
-            [['FECH_LLEGA', 'FECH_CORTE', 'FECH_DIGI', 'FECH_MODI', 'FECH_ELIM', 'NUMERO_GUIA'], 'safe'],
+            [['FECH_LLEGA', 'FECH_CORTE'], 'required'],
+            [['FECH_LLEGA', 'FECH_CORTE', 'FECH_DIGI', 'FECH_MODI', 'FECH_ELIM','NUMERO_GUIA'], 'safe'],
             [['NUM_OBRA', 'NUM_GUIA'], 'string', 'max' => 12],
             [['USU_DIGI', 'USU_MODI', 'USU_ELIM'], 'string', 'max' => 45],
             [['COD_ESTA'], 'string', 'max' => 1],
